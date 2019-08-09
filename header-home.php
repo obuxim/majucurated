5891cd518ea1284d7e80c978488425e8cac63f6b
Zubair Hasan Zubo <obuxim@gmail.com>
2019-08-09 19:51:50 +0000

    Menu Finished (Menu Responsive Not Found)

--- header-home.php
+++ header-home.php
@@ -39,27 +39,59 @@
             <li class="nav-item"><a class="nav-link text-light" href="about.php">about us</a></li>
             <li class="nav-item"><a class="nav-link text-light" href="blog.php">blog</a></li>
         </ul>
-        <a onclick=toggleNav(1) id="navToggle"><img src="img/menu-light.png" alt="Menu" /></a>
-        <div id="menu-1" class="fixed-top w-100 h-100 bg-white text-dark d-none flex-column">
+        <a onclick=toggleNav() id="navToggle"><img src="img/menu-light.png" alt="Menu" /></a>
+    </header>
+    <div id="menu" class="fixed-top p-5 w-100 h-100 bg-white text-dark d-none flex-column justify-content-between">
+        <div class="container-fluid">
+            <div class="d-flex align-items-center justify-content-between p-2">
+                <a href="index.php" class="navbar-brand"><img src="img/logo-dark.png" alt="Maju Curated" /></a>
+                <a onclick=toggleNav() id="navToggle"><i class="fas fa-times fa-2x" ></i></a>
+            </div>
+        </div>
+        <div>
             <div class="container">
-                <div class="d-flex align-items-center justify-content-between p-2">
-                    <a href="index.php" class="navbar-brand"><img src="img/logo-dark.png" alt="Maju Curated" /></a>
-                    <a onclick=toggleNav(1) id="navToggle"><i class="fas fa-times fa-2x" ></i></a>
+                <div class="row">
+                    <div class="col-3 d-flex justify-content-center">
+                        <div class="d-flex flex-column">
+                            <a class="menu-header text-uppercase">home</a>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex justify-content-center">
+                        <div class="d-flex flex-column">
+                            <a class="menu-header text-uppercase">production</a>
+                            <a class="menu-item text-capitalize">photography</a>
+                            <a class="menu-item text-capitalize">videography</a>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex justify-content-center">
+                        <div class="d-flex flex-column">
+                            <a class="menu-header text-uppercase">design</a>
+                            <a class="menu-item text-capitalize">digital design</a>
+                            <a class="menu-item text-capitalize">branding</a>
+                            <a class="menu-item text-capitalize">marketing strategy</a>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex justify-content-center">
+                        <div class="d-flex flex-column">
+                            <a class="menu-header text-uppercase">about us</a>
+                            <a class="menu-item text-capitalize">contact us</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </header>
+        <div>
+            <div class="container border-top border-dark d-flex justify-content-center">
+                <a href="https://instagram.com/majucurated" class="mt-5 mx-5"><img src="img/social/instagram.png" class="img-fluid" /></a>
+                <a href="https://facebook.com/majucurated" class="mt-5 mx-5"><img src="img/social/facebook.png" class="img-fluid" /></a>
+                <a href="https://pinterest.com/majucurated" class="mt-5 mx-5"><img src="img/social/pinterest.png" class="img-fluid" /></a>
+                <a href="https://youtube.com/majucurated" class="mt-5 mx-5"><img src="img/social/youtube.png" class="img-fluid"></a>
+            </div>
+        </div>
+    </div>
     <header class="d-flex d-md-none align-items-center justify-content-between container sticky-top bg-white pl-2 pr-3 text-dark text-uppercase">
         <a href="index.php" class="navbar-brand"><img src="img/logo-dark.png" alt="Maju Curated" /></a>
-        <a onclick=toggleNav(2) id="navToggle"><img src="img/menu-dark.png" alt="Menu" /></a>
-        <div id="menu-2" class="fixed-top w-100 h-100 bg-white text-dark d-none flex-column">
-            <div class="container">
-                <div class="d-flex align-items-center justify-content-between p-2">
-                    <a href="index.php" class="navbar-brand"><img src="img/logo-dark.png" alt="Maju Curated" /></a>
-                    <a onclick=toggleNav(2) id="navToggle"><i class="fas fa-times fa-2x" ></i></a>
-                </div>
-            </div>
-        </div>
+        <a onclick=toggleNav() id="navToggle"><img src="img/menu-dark.png" alt="Menu" /></a>
     </header>
     <section id="home-slider" class="carousel slide align-itmes-center d-flex" data-ride="carousel">
         <ol class="carousel-indicators">
