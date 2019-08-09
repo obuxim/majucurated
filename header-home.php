27c0c839a763c7c3f2c6a74014d8b58dccb8bf81
Zubair Hasan Zubo <obuxim@gmail.com>
2019-08-09 05:41:25 +0000

    Initialized

---
+++ header-home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    // Essential Meta Tags
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    // SEO Meta Tags
+    <title>Maju Curated</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Zubair Hasan">
+    <meta copyright="Octoriz Private Limited">
+    // Including Bootstrap
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    // Including Google Font (Lato)
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    // Including FontAwesome Kit
+    <script src="https://kit.fontawesome.com/1c781ab882.js"></script>
+    // Including Custom Stylesheet
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("header-home.php"); ?>
+
+<h1>Homepage</h1>
+
+<?php include("footer.php"); ?>
