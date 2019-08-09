a2990a9bff7b206e1664ba6885b3a3f2f431ebf8
Zubair Hasan Zubo <obuxim@gmail.com>
2019-08-09 07:02:56 +0000

    Homepage Slider Finished

--- header-home.php
+++ header-home.php
@@ -1,22 +1,77 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-    // Essential Meta Tags
+    <!-- Essential Meta Tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    // SEO Meta Tags
+    <!-- SEO Meta Tags -->
     <title>Maju Curated</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="author" content="Zubair Hasan">
     <meta copyright="Octoriz Private Limited">
-    // Including Bootstrap
+    <!-- Including Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    // Including Google Font (Lato)
+    <!-- Including Google Font (Lato) -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-    // Including FontAwesome Kit
+    <!-- Including FontAwesome Kit -->
     <script src="https://kit.fontawesome.com/1c781ab882.js"></script>
-    // Including Custom Stylesheet
+    <!-- Including Custom CSS -->
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
+    <header class="d-flex align-items-center justify-content-around fixed-top bg-transparent p-2 text-light text-uppercase border-bottom border-white">
+        <a href="index.php" class="navbar-brand"><img src="img/logo-light.png" alt="Maju Curated" /></a>
+        <ul class="nav">
+            <li class="nav-item"><a class="nav-link text-light" href="index.php">home</a></li>
+            <li class="nav-item"><a class="nav-link text-light" href="photography.php">photography</a></li>
+            <li class="nav-item"><a class="nav-link text-light" href="videography.php">videography</a></li>
+            <li class="nav-item"><a class="nav-link text-light" href="design.php">design</a></li>
+            <li class="nav-item"><a class="nav-link text-light" href="shop.php">shop</a></li>
+            <li class="nav-item"><a class="nav-link text-light" href="about.php">about us</a></li>
+            <li class="nav-item"><a class="nav-link text-light" href="blog.php">blog</a></li>
+        </ul>
+        <a onclick=toggleNav() id="navToggle"><img src="img/menu-light.png" alt="Menu" /></a>
+        <div id="menu" class="fixed-top w-100 h-100 bg-white text-dark d-none flex-column">
+            <div class="container">
+                <div class="d-flex align-items-center justify-content-between p-2">
+                    <a href="index.php" class="navbar-brand"><img src="img/logo-dark.png" alt="Maju Curated" /></a>
+                    <a onclick=toggleNav() id="navToggle"><i class="fas fa-times fa-2x" ></i></a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="home-slider" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#home-slider" data-slide-to="0" class="active"></li>
+            <li data-target="#home-slider" data-slide-to="1"></li>
+            <li data-target="#home-slider" data-slide-to="2"></li>
+            <li data-target="#home-slider" data-slide-to="3"></li>
+            <li data-target="#home-slider" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/home/slider/1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/slider/2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/slider/3.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/slider/4.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/home/slider/5.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#home-slider" role="button" data-slide="prev">
+            <img src="img/home/arrow_left.png" alt="Previous" />
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#home-slider" role="button" data-slide="next">
+            <img src="img/home/arrow_right.png" alt="Next" />
+            <span class="sr-only">Next</span>
+        </a>
+    </section>
---
+++ js/script.js
@@ -0,0 +1,15 @@
+let currentMenuState = 0
+const toggleNav = function(){
+    const menuElement = document.getElementById("menu")
+    if (currentMenuState == 0) {
+        menuElement.classList.remove("d-none")
+        menuElement.classList.add("d-flex")
+        console.log("Menu opened")
+        currentMenuState = 1
+    } else {
+        menuElement.classList.remove("d-flex")
+        menuElement.classList.add("d-none")
+        console.log("Menu closed")
+        currentMenuState = 0
+    }
+}
